f17dd6d59469737f2b8d16a677171ba62fe7e4dc
Fathiya Yoosef <155487698+Dhool143@users.noreply.github.com>
2026-02-21 07:04:49 -0800

    Final clean commit

---
+++ my-nextjs-app/app/page.tsx
@@ -0,0 +1,45 @@
+
+
+   export default function Home () {
+
+    return (
+
+   <main style ={{ padding: "40px", "fontFamily": "Arial"}}>
+
+    <header>
+      <h1 style={{ fontSize: "32px"}}> 
+       My next.js Home Page
+      </h1>
+    </header>
+    
+    <section style={{ marginTop: "20px"}}> 
+    <h2>About</h2>
+
+    <p>This Project Demonstrates Next.js</p>
+
+    </section>
+
+    <section>
+   <h2>Features</h2>
+   <ul>
+    <li>File-based routing</li>
+    <li>Static Site Generation</li>
+    <li>Dynamic Routes</li>
+
+   </ul>
+
+    </section>
+    
+    <footer>
+
+   <p>© 2026 Fathiya Yoosef</p>
+
+    </footer>
+
+   </main>
+
+
+    );
+
+  
+}
